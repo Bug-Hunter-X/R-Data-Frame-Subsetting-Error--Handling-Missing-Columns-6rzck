27607931edd4384aa50ddc4a11d14d84983456c7
BugHunter-X <askzenai@gmail.com>
2025-01-09 18:12:05 +0100

    Add bug.R file

---
+++ bug.R
@@ -0,0 +1,11 @@
+```R
+# This code attempts to subset a data frame using a character vector of column names,
+# but it fails if any of the specified column names are not present in the data frame.
+
+df <- data.frame(a = 1:3, b = 4:6, c = 7:9)
+cols_to_select <- c("a", "b", "d")  # 'd' is not a column in df
+
+subset_df <- df[, cols_to_select]
+
+print(subset_df) # This will produce an error
+```
